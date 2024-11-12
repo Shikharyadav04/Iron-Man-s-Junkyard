59f5ffedc1c9a7f66b529ea2d59136180c877168
Shikhar Yadav <shikharyadav645@gmail.com>
2024-11-13 00:06:47 +0530

    subscription check middle ware added

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -15,7 +15,7 @@ import {
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-
+import { checkSubscription } from "../middlewares/subscription.middleware.js";
 const router = Router();
 
 // Updated route to handle both file and other fields in form-data
@@ -27,7 +27,7 @@ router.route("/register").post(
 );
 
 // Other routes
-router.route("/login").post(loginUser);
+router.route("/login").post(checkSubscription, loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refreshToken").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
